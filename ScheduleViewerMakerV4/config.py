d7ddd362efabbd3d1d2d0aa9d428ef8b668b9c34
Rein <99861894+Rein-C@users.noreply.github.com>
2024-10-23 00:15:00 +0800

    Delete ScheduleViewerMakerV4 directory

--- ScheduleViewerMakerV4/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SECRET_KEY = os.urandom(32)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///schedule_viewer.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- ScheduleViewerMakerV4/forms/__init__.py
+++
@@ -1,5 +0,0 @@
-from .login import LoginForm
-from .signup import SignupForm
-from .schedule import ScheduleForm
-
-__all__ = ['LoginForm', 'SignupForm', 'ScheduleForm']
